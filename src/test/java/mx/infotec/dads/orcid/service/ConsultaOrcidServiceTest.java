3d5175c41710356f6567399290b1d474907c645a
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2016-09-08 14:39:34 -0500

    Update
    Update the format of the file.

--- src/test/java/mx/infotec/dads/orcid/service/ConsultaOrcidServiceTest.java
+++ src/test/java/mx/infotec/dads/orcid/service/ConsultaOrcidServiceTest.java
@@ -20,48 +20,48 @@ import mx.infotec.dads.orcid.service.impl.DefaultCredencialsManagerServiceImpl;
  *
  */
 public class ConsultaOrcidServiceTest {
-	private static final Logger LOGGER = LoggerFactory.getLogger(ConsultaOrcidServiceTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsultaOrcidServiceTest.class);
 
-	private ConsultaOrcidService consultaOrcidService;
+    private ConsultaOrcidService consultaOrcidService;
 
-	@BeforeTest
-	public void beforeTest() {
-		CredentialManagerService credentialManagerService = new DefaultCredencialsManagerServiceImpl();
-		consultaOrcidService = new ConsultaOrcidServiceImpl(credentialManagerService);
-	}
+    @BeforeTest
+    public void beforeTest() {
+        CredentialManagerService credentialManagerService = new DefaultCredencialsManagerServiceImpl();
+        consultaOrcidService = new ConsultaOrcidServiceImpl(credentialManagerService);
+    }
 
-	@Test
-	public void consultaPorId() throws OrcidServiceException {
-		PersonaGenerica persona = consultaOrcidService.consultaPorId("0000-0001-5276-9864");
-		if (persona != null) {
-			LOGGER.info("Fecha: " + persona.getFechaNacCons());
-			LOGGER.info("idOrcid: " + persona.getIdOrcid());
-			LOGGER.info("idPersona: " + persona.getIdPersona());
-			LOGGER.info("idPersonaConacyt: " + persona.getIdPersonaConacyt());
-			LOGGER.info("idTipoIdentificador: " + persona.getIdTipoIdentificador());
-			LOGGER.info("identificador: " + persona.getIdentificador());
-			LOGGER.info("nombres: " + persona.getNombres());
-			LOGGER.info("primerApellido: " + persona.getPrimerApellido());
-			LOGGER.info("tipoPersona: " + persona.getTipoPersona());
-			LOGGER.info("tipoPersona: " + persona.getTipoPersona());
-		}
-	}
+    @Test
+    public void consultaPorId() throws OrcidServiceException {
+        PersonaGenerica persona = consultaOrcidService.consultaPorId("0000-0001-5276-9864");
+        if (persona != null) {
+            LOGGER.info("Fecha: " + persona.getFechaNacCons());
+            LOGGER.info("idOrcid: " + persona.getIdOrcid());
+            LOGGER.info("idPersona: " + persona.getIdPersona());
+            LOGGER.info("idPersonaConacyt: " + persona.getIdPersonaConacyt());
+            LOGGER.info("idTipoIdentificador: " + persona.getIdTipoIdentificador());
+            LOGGER.info("identificador: " + persona.getIdentificador());
+            LOGGER.info("nombres: " + persona.getNombres());
+            LOGGER.info("primerApellido: " + persona.getPrimerApellido());
+            LOGGER.info("tipoPersona: " + persona.getTipoPersona());
+            LOGGER.info("tipoPersona: " + persona.getTipoPersona());
+        }
+    }
 
-//	@Test
-	public void consultaPorIdList() throws OrcidServiceException {
-		List<PersonaGenerica> personas = consultaOrcidService.consultaListaPorId("0000");
-		for (PersonaGenerica persona : personas) {
-			if (persona != null) {
-				LOGGER.info("Fecha: " + persona.getFechaNacCons());
-				LOGGER.info("idOrcid: " + persona.getIdOrcid());
-				LOGGER.info("idPersona: " + persona.getIdPersona());
-				LOGGER.info("idPersonaConacyt: " + persona.getIdPersonaConacyt());
-				LOGGER.info("idTipoIdentificador: " + persona.getIdTipoIdentificador());
-				LOGGER.info("identificador: " + persona.getIdentificador());
-				LOGGER.info("nombres: " + persona.getNombres());
-				LOGGER.info("primerApellido: " + persona.getPrimerApellido());
-				LOGGER.info("tipoPersona: " + persona.getTipoPersona());
-			}
-		}
-	}
+    // @Test
+    public void consultaPorIdList() throws OrcidServiceException {
+        List<PersonaGenerica> personas = consultaOrcidService.consultaListaPorId("0000");
+        for (PersonaGenerica persona : personas) {
+            if (persona != null) {
+                LOGGER.info("Fecha: " + persona.getFechaNacCons());
+                LOGGER.info("idOrcid: " + persona.getIdOrcid());
+                LOGGER.info("idPersona: " + persona.getIdPersona());
+                LOGGER.info("idPersonaConacyt: " + persona.getIdPersonaConacyt());
+                LOGGER.info("idTipoIdentificador: " + persona.getIdTipoIdentificador());
+                LOGGER.info("identificador: " + persona.getIdentificador());
+                LOGGER.info("nombres: " + persona.getNombres());
+                LOGGER.info("primerApellido: " + persona.getPrimerApellido());
+                LOGGER.info("tipoPersona: " + persona.getTipoPersona());
+            }
+        }
+    }
 }
